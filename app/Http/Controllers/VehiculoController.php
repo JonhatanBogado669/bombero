9517243035a8c916d58aecd2332d69323697de36
Jonhatan Bogado <jonhatanbogado@gmail.com>
2021-11-02 23:36:10 -0300

    correccion error index VehiculoController

--- app/Http/Controllers/VehiculoController.php
+++ app/Http/Controllers/VehiculoController.php
@@ -16,10 +16,10 @@ class VehiculoController extends Controller
      */
     public function index()
     {
-        $data=Vehiculo::join('estado as e','vehiculo as v','e.id','=','v.estado_id')
-                ->join('tipo_vehiculo as t','t.id','=','v.tipo_vehiculo_id')
-                ->orderBy('v.id','ASC')
-                ->get(['v.id','v.patente','v.codigo','e.id as idestado','e.descripcion as estado',
+        $data=Vehiculo::join('estado as e','e.id','=','vehiculo.estado_id')
+                ->join('tipo_vehiculo as t','t.id','=','vehiculo.tipo_vehiculo_id')
+                ->orderBy('vehiculo.id','ASC')
+                ->get(['vehiculo.id','vehiculo.patente','vehiculo.codigo','e.id as idestado','e.descripcion as estado',
                 't.id as idtipo','t.descripcion as tipo']);
         return view('vehiculo.index', ['data'=>$data]);
     }
